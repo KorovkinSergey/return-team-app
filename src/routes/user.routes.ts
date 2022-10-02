e3b9d57f7062ea5c543a2cab19349214b94c7a80
korovkinsergey <ko.sergey@outlook.com>
2022-10-02 14:50:57 +0300

    add groupList

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,11 +6,11 @@ const router = Router()
 
 router.get('/:id', [auth], async (req: any, res: any) => {
 	try {
-		const user = User.findById({ _id: req.params.id })
+		const user = await User.findOne({ id: req.params.id })
 
 		if (!user) return res.status(404).json({ message: 'Пользователь не найден' })
 
-		res.status(201).json({ user })
+		res.status(200).json(user)
 	} catch (e) {
 		console.log('res', e)
 		res.status(500).json({ message: 'Что-то пошло не так, попробуй снова' })
